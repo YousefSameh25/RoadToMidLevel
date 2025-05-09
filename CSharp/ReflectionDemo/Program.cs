8558d84bfe5905f621ff343469d06b4918094015
Yousef Sameh <yousefsamehmahmoud@gmail.com>
2025-05-09 15:36:25 +0300

    CSharp: Reflection Add reflection demo

---
+++ CSharp/ReflectionDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace ReflectionDemo
+{
+    internal class Program
+    {
+        #region Helper Methods
+        static int GetCurrentLine([CallerLineNumber] int lineNumber = 0)
+        {
+            return lineNumber;
+        }
+        static void PrintLine(int lineLength = 100)
+        {
+            Console.WriteLine(new string('=', lineLength));
+        }
+        static void Process(Action reflectionAction, string functionName, int line)
+        {
+            Console.WriteLine($"Function Name: {functionName} at Line {line}");
+            Console.Write("Output: "); reflectionAction();
+            PrintLine();
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region Reflection Definition
+            /*
+                = What is reflection?
+                    - Reflection is the process of getting an assembly's meta data at [Runtime].
+                    - Also it give the power of creating new properties at runtime!
+
+                = What kind of meta data?
+                    - Data type of any programming component (Class, Struct, etc)
+                    - Properties inside type.
+             */
+            #endregion
+
+            Type sampleType = typeof(Sample);
+
+            #region Reflection Functions
+            Process(() => { Console.WriteLine(DateTime.Now.GetType()); }, "DateTime.Now.GetType()", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.Namespace); }, "sampleType.Namespace", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.Name); }, "sampleType.Name", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.FullName); }, "sampleType.FullName", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.BaseType); }, "sampleType.BaseType", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.IsAbstract); }, "sampleType.IsAbstract", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.IsPublic); }, "sampleType.IsPublic", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.IsNotPublic); }, "sampleType.IsNotPublic", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.IsPrimitive); }, "sampleType.IsPrimitive", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.IsInterface); }, "sampleType.IsInterface", GetCurrentLine());
+            Process(() => { Console.WriteLine(sampleType.Assembly); }, "sampleType.Assembly", GetCurrentLine());
+            Process(() => { foreach (var prop in sampleType.GetProperties()) { Console.WriteLine(prop.Name); } }, "sampleType.GetProperties()", GetCurrentLine());
+            Process(() => { foreach (var method in sampleType.GetMethods(BindingFlags.DeclaredOnly)) { Console.WriteLine(method.Name); } }, "sampleType.GetMethods(BindingFlags.DeclaredOnly)", GetCurrentLine());
+            #endregion
+        }
+    }
+}
---
+++ CSharp/ReflectionDemo/Sample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class Sample
+    {
+        // Private field
+        private string _secret = "hidden";
+
+        // Public field
+        public string PublicNote = "visible";
+
+        // Property
+        public int Number { get; set; }
+
+        // Constructor
+        public Sample(int number)
+        {
+            Number = number;
+        }
+
+        // Instance method
+        public string Describe()
+        {
+            return $"Number is {Number}, secret is {_secret}, note is {PublicNote}.";
+        }
+
+        // Static method
+        public static string GetClassInfo()
+        {
+            return "This is the Sample class.";
+        }
+    }
+
+}
